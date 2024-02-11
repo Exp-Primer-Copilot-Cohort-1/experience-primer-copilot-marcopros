3a3287841ec2c2c1d28a39fc19897ffd25e941fe
Marco Prosperi <106628127+marcopros@users.noreply.github.com>
2024-02-11 11:15:14 +0000

    Third Copilot commit

--- comments.js
+++ comments.js
@@ -1,30 +1,14 @@
-// Create a web server 
-// 1. Create a web server that listens on port 3000
-// 2. When you visit the root URL, it should display "Hello, World!"
-// 3. When you visit the URL /cats, it should display "Meow!"
-// 4. When you visit the URL /dogs, it should display "Woof!"
-// 5. When you visit the URL /cats_and_dogs, it should display "Dogs and cats living together...mass hysteria!!"
+// server.js
+const http = require('http');
 
-var http = require('http');
-var url = require('url');
+const server = http.createServer((req, res) => {
+  // Handling incoming requests
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!\n');
+});
 
-http.createServer(function(req, res) {
-    var path = url.parse(req.url).pathname;
-    if (path === '/') {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Hello, World!');
-    } else if (path === '/cats') {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Meow!');
-    } else if (path === '/dogs') {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Woof!');
-    } else if (path === '/cats_and_dogs') {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('Dogs and cats living together...mass hysteria!!');
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end('Page not found');
-    }
-}).listen(3000);
-console.log('Server started on localhost:3000; press Ctrl-C to terminate...');
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
